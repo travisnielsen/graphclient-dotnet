469c6fccc098954da820941e0666dd7a3fca3d51
Travis Nielsen <trniel@microsoft.com>
2020-01-30 12:00:06 -0600

    initial commmit

---
+++ src/Models/AppSettingsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupsApiB2C.Models
+{
+    public class AppSettingsModel
+    {
+        public string Tenant { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ src/Models/OutputClaimsModel.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroupsApiB2C.Models
+{
+    public class OutputClaimsModel
+    {
+        // Demo: List of security groups the user is member of
+        public List<string> groups { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
